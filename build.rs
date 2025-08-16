24b9b1310f1293ea5f6d6293cbec02baf194ac99
Hyunsik Choi <hyunsik@furiosa.ai>
2025-08-17 08:08:49 +0900

    Turn off the python binding target for no libpython.so dependency

--- build.rs
+++ build.rs
@@ -1,7 +1,26 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+/// Write a custom config.cmake
+fn generate_config_cmake() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not set"));
+    let cmake_build_dir = out_dir.join("build");
+    let _ = fs::create_dir_all(&cmake_build_dir);
+    let path = cmake_build_dir.join("config.cmake");
+    // Turn off Python bindings because we don't need and we don't want to rely on libpython.so.
+    let contents = "set(XGRAMMAR_BUILD_PYTHON_BINDINGS OFF CACHE BOOL \"\" FORCE)\n";
+    fs::write(&path, contents).expect("write config.cmake failed");
+}
+
 fn main() {
+    generate_config_cmake();
+
     // Running the CMake build for the xgrammar library
-    let xgrammar =
-        cmake::Config::new("thirdparty/xgrammar").define("CMAKE_CXX_COMPILER", "clang++").build();
+    let xgrammar = cmake::Config::new("thirdparty/xgrammar")
+        .define("CMAKE_CXX_COMPILER", "clang++")
+        .build_target("xgrammar")
+        .build();
 
     // Specifying the path to link the built library
     println!("cargo:rustc-link-search=native={}/build", xgrammar.display());
@@ -11,14 +30,15 @@ fn main() {
     let mut config = cpp_build::Config::new();
     config
         .include("thirdparty/xgrammar/include/")
+        // List all thirdparty libraries' headers
         .include("thirdparty/xgrammar/3rdparty/dlpack/include/")
         .include("thirdparty/xgrammar/3rdparty/picojson/")
         .flag("-std=c++17");
-    config.build("src/xgrammar/xgrammar.rs");
+    config.build("src/lib.rs");
 
     // Re-run if any of these files change
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src/xgrammar/xgrammar.rs");
+    println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=thirdparty/xgrammar/include/xgrammar/");
     println!("cargo:rerun-if-changed=thirdparty/xgrammar/3rdparty/dlpack/include/dlpack/");
     println!("cargo:rerun-if-changed=thirdparty/xgrammar/3rdparty/picojson/");
