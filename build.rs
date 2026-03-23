4f1bd95657edf7bd339e590c140941ad3a693888
Sukchul Cho <sukchul.cho@furiosa.ai>
2026-03-23 06:40:14 +0000

    Unset CMAKE_CXX_COMPILER
    Unset CMAKE_CXX_COMPILER to properly support -flto on AArch64.

--- build.rs
+++ build.rs
@@ -20,7 +20,6 @@ fn main() {
     // Note: -Wno-deprecated-declarations is needed because xgrammar uses std::bind internally,
     // which triggers deprecation warnings for std::result_of in C++17 with newer compilers.
     let xgrammar = cmake::Config::new("thirdparty/xgrammar")
-        .define("CMAKE_CXX_COMPILER", "clang++")
         .define("CMAKE_CXX_FLAGS", "-Wno-deprecated-declarations")
         .build_target("xgrammar")
         .build();
